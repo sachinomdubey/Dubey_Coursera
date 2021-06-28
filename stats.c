b5c2cc98cb4ab6c18d04c9104c09fb5bb314a755
root <root@LAPTOP-LT4RQ6PC.localdomain>
2021-06-28 05:31:55 +0530

    Creating Inital coursera repository

---
+++ stats.c
@@ -0,0 +1,115 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file stats.c
+ * @brief This a program file to compute the stats of a given array
+ *
+ * <Add Extended Description Here>
+ *
+ * @author Sachinkumar Dubey
+ * @date 27 June 2021
+ *
+ */
+
+
+#include <stdio.h>
+#include "stats.h"
+
+/* Size of the Data Set */
+#define SIZE (40)
+
+//Declaring the functions unsigned
+void print_statistics(int arr[],int N);
+void print_array(int arr[],int N);
+float find_median(int arr[],int N);
+float find_mean(int arr[],int N);
+int find_maximum(int arr[],int N);
+int find_minimum(int arr[], int N);
+void sort_array(int arr[],int N);
+
+
+void main() {
+
+  unsigned int test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+                              114, 88,   45,  76, 123,  87,  25,  23,
+                              200, 122, 150, 90,   92,  87, 177, 244,
+                              201,   6,  12,  60,   8,   2,   5,  67,
+                                7,  87, 250, 230,  99,   3, 100,  90};
+  print_array(test,40);
+  sort_array(test,40);
+  print_statistics(test,40);
+  /* Other Variable Declarations Go Here */
+  /* Statistics and Printing Functions Go Here */
+
+}
+
+void print_statistics(int arr[],int N)
+{
+  printf("Maximum = %d\nMinimum=%d\nMean=%0.1f\nMedian=%0.1f\n",find_maximum(arr,N),find_minimum(arr,N),find_mean(arr,N),find_median(arr,N));
+}
+
+void print_array(int arr[],int N)
+{
+    printf("Printing the given array :\n");
+  for(int i=0; i<N; i++)
+  {
+    printf("%d ",arr[i]);
+  }
+  printf("\n");
+}
+
+float find_median(int arr[],int N)
+{
+  if (N % 2 == 0)
+  return (arr[N/2]+arr[N/2-1])/2;
+  else
+  return arr[(N-1)/2]; 
+}
+
+float find_mean(int arr[],int N)
+{
+  int sum=0;
+  for(int i=0; i<N; i++)
+  {
+    sum+=arr[i];
+  }
+  return sum/N;
+}
+
+int find_maximum(int arr[],int N)
+{
+  return arr[0];
+}
+
+int find_minimum(int arr[], int N)
+{
+  return arr[N-1];
+}
+
+void sort_array(int arr[],int N)
+{
+	int temp;
+  for(int i=0; i<N; i++)
+  {
+    for(int j=0; j<N; j++)
+    {
+      if(arr[j]<arr[j+1])
+	  {
+		  temp=arr[j];
+		  arr[j]=arr[j+1];
+		  arr[j+1] = temp;
+	  }
+    }
+  }
+  
+}
+
+/* Add other Implementation File Code Here */
---
+++ stats.h
@@ -0,0 +1,82 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file stats.h 
+ * @brief Contains descriptions of each functions used in stats.c program
+ *
+ * <Add Extended Description Here>
+ *
+ * @author Sachinkumar Dubey
+ * @date 27 June 2021
+ *
+ */
+#ifndef __STATS_H__
+#define __STATS_H__
+
+
+/**
+ * @brief A function that prints the statistics of an array including minimum, maximum, mean, and median.
+ * @param Arr - Array for which the statistics are to be printed.
+ * @param N - Denotes the size of the input array.
+ * @return return nothing, just prints the various statistics.
+ */
+void print_statistics(int arr[],int N);
+
+/**
+ * @brief Given an array of data and a length, prints the array to the screen.
+ * @param Arr - Array for which the statistics are to be printed.
+ * @param N - Denotes the size of the input array.
+ * @return Return nothing, just prints the array.
+ */
+void print_array(int arr[],int N);
+
+/**
+ * @brief Given an array of data and a length, returns the median value.
+ * @param Arr - Array for which the statistics are to be printed.
+ * @param N - Denotes the size of the input array.
+ * @return returns the median of the array which is a float value.
+ */
+float find_median(int arr[],int N);
+
+/**
+ * @brief Given an array of data and a length, returns the mean value.
+ * @param Arr - Array for which the statistics are to be printed.
+ * @param N - Denotes the size of the input array.
+ * @return returns the mean of the array which is a float value.
+ */
+float find_mean(int arr[],int N);
+
+/**
+ * @brief Given an array of data and a length, returns the maximum value.
+ * @param Arr - Array for which the statistics are to be printed.
+ * @param N - Denotes the size of the input array.
+ * @return returns the maximum value of the array which is a int value.
+ */
+int find_maximum(int arr[],int N);
+
+/**
+ * @brief Given an array of data and a length, returns the minimum value.
+ * @param Arr - Array for which the statistics are to be printed.
+ * @param N - Denotes the size of the input array.
+ * @return returns the minimum value of the array which is a int value.
+ */
+int find_minimum(int arr[], int N);
+
+/**
+ * @brief Given an array of data and a length, sorts the array from largest to smallest.  
+ * (The zeroth Element should be the largest value, and the last element (n-1) should be the smallest value. )
+ * @param Arr - Array for which the statistics are to be printed.
+ * @param N - Denotes the size of the input array.
+ * @return return nothing, Just sorts the given array from largest to smallest.
+ */
+void sort_array(int arr[],int N);
+
+#endif /* __STATS_H__ */
